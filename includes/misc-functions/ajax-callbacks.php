e7c2765517ca6f30d9aa688a0711a79b0ad22684
Phil Johnston <support@mintplugins.com>
2017-02-01 11:09:37 -0500

    Update to use MailChimp API V3

--- includes/misc-functions/ajax-callbacks.php
+++ includes/misc-functions/ajax-callbacks.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Ajax callback functions for the MP Stacks Mailchimp add-on
  *
@@ -21,41 +21,43 @@
  * @return   void
  */
 function mp_stacks_mailchimp_add_user(){
-	
+
 	if ( !isset( $_POST['mp_stacks_mailchimp_email'] ) || !isset( $_POST['mp_stacks_mailchimp_brick_id'] ) ){
-		
+
 		echo __( "Oops! Something went wrong", 'mp_stacks_mailchimp' );
 		die();
-			
+
 	}
-	
+
 	$post_id = $_POST['mp_stacks_mailchimp_brick_id'];
 	$mailchimp_email = $_POST['mp_stacks_mailchimp_email'];
-	
+
 	//Mailchimp list info stored in post
 	$mailchimp_api_key = get_post_meta($post_id, 'mailchimp_api_key', true);
 	$mailchimp_list_id = get_post_meta($post_id, 'mailchimp_list_id', true);
 	$mailchimp_success_message = mp_core_get_post_meta($post_id, 'mailchimp_success_message', __( 'Thanks for joining! Check your email to confirm.', 'mp_stacks_mailchimp' ) );
 	$submit_button_text = mp_core_get_post_meta( $post_id, 'mailchimp_submit_button_text', __('Join', 'mp_stacks_mailchimp' ) );
-	
+
 	//Get the size of the email input field
 	$mailchimp_email_input_field_size = get_post_meta( $post_id, 'mailchimp_email_input_field_size', true);
 	$mailchimp_email_input_field_size = empty( $mailchimp_email_input_field_size ) ? '35' : $mailchimp_email_input_field_size;
-			
+
+	/*
+
 	//Mail chimp
 	$mailchimp_sever_number = explode( '-', $mailchimp_api_key );
 	$mailchimp_sever_number = $mailchimp_sever_number[1];
-	
-	$json_for_mailchimp_request = json_encode( array( 
+
+	$json_for_mailchimp_request = json_encode( array(
 		"apikey" => $mailchimp_api_key,
 		"id" => $mailchimp_list_id,
 		"email" => array(
 			"email" => $mailchimp_email
 		),
 	) );
-	
+
 	$mailchimp_url = 'https://'.$mailchimp_sever_number.'.api.mailchimp.com/2.0/lists/subscribe/';
-	
+
 	$response = wp_remote_post( $mailchimp_url, array(
 		'method' => 'POST',
 		'timeout' => 45,
@@ -69,21 +71,26 @@ function mp_stacks_mailchimp_add_user(){
 		'cookies' => array()
 		)
 	);
-	
+	*/
+
+	$data_for_mailchimp = array(
+	    'email'    => $mailchimp_email,
+	    'status'   => 'subscribed',
+	);
+
+	$response = mp_stacks_mailchimp_maybe_add_member( $data_for_mailchimp, $mailchimp_api_key, $mailchimp_list_id );
+
 	if ( is_wp_error( $response ) ){
-		
+
 		echo __('OOPS! API and List Keys for this mailinglist may not yet been configured.', 'mp_stacks_mailchimp');
-		print_r($response);
 		die();
-			
+
 	}
-	
-	$response = json_decode($response['body']);
-				
-	if ( isset( $response->status ) ){
-		
-		echo '<div class="mp-stacks-mailchimp-error">' . $response->error . '</div>
-	
+
+	if ( isset( $response['error'] ) ){
+
+		echo '<div class="mp-stacks-mailchimp-error">' . $response['error'] . '</div>
+
 		<fieldset>
 			<div class="mp-stacks-mailchimp-sign-up-container">
 				<input value="" class="mp-stacks-mailchimp-email" name="mp_stacks_mailchimp_email" size="100" type="text" placeholder="' . __('Email Address', 'mp_stacks_mailchimp' ) . '"/>
@@ -91,15 +98,15 @@ function mp_stacks_mailchimp_add_user(){
 				<div class="mp_stacks_mailchimp_submit"><div class="mp_stacks_mailchimp_submit_text">' . $submit_button_text . '</div></div>
 			</div>
 		</fieldset>';
-		
+
 	}
 	else{
-		
+
 		echo '<div class="mp-stacks-mailchimp-success">' . $mailchimp_success_message . '</div>';
 	}
-	
+
 	die();
-		
+
 }
 add_action('wp_ajax_mp_stacks_mailchimp', 'mp_stacks_mailchimp_add_user');
 add_action('wp_ajax_nopriv_mp_stacks_mailchimp', 'mp_stacks_mailchimp_add_user');
--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -11,3 +11,98 @@
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @author     Philip Johnston
  */
+
+ function mp_stacks_mailchimp_maybe_add_member( $data, $api_key, $list_id ) {
+
+	$member_id = md5( strtolower( $data['email'] ) );
+
+	//Mail chimp
+ 	$mailchimp_sever_number = explode( '-', $api_key );
+ 	$mailchimp_sever_number = $mailchimp_sever_number[1];
+
+	//Check if the user is already on the list
+	$url = 'https://' . $mailchimp_sever_number . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/' . $member_id;
+
+	$get_request_args = array(
+		'headers' => array(
+			'Authorization' => 'Basic ' . base64_encode( 'username:' . $api_key )
+		),
+		'sslverify'   => true,
+	);
+
+	$response_1 = wp_remote_get( $url, $get_request_args );
+
+	if ( is_wp_error( $response_1 ) ){
+		return __( 'There was a problem with the request. Please try again.', 'mp_stacks_mailchimp' );
+	}
+
+	$response_1 = json_decode( wp_remote_retrieve_body( $response_1 ), true );
+
+	if ( !isset( $response_1['status'] ) ){
+		return array(
+			'error' => __( 'No status found. Please try again.', 'mp_stacks_mailchimp' )
+		);
+	}
+
+	if ( $response_1['status'] == 'subscribed' ){
+		return array(
+			'error' => __( 'You are already subscribed to this list', 'mp_stacks_mailchimp' )
+		);
+	}
+
+	if ( $response_1['status'] == 'unsubscribed' ){
+		return array(
+			'error' => __( 'You unsubscribed from this list. Unfortunately, you can not re-join it.', 'mp_stacks_mailchimp' )
+		);
+	}
+
+	if ( $response_1['status'] == 'pending' ){
+		return __( 'Check your email to confirm!', 'mp_stacks_mailchimp' );
+	}
+
+	// If the user's status is something other than 404 at this time
+	if ( $response_1['status'] != '404' ){
+		return __( 'Something went wrong with the request. Please try again. User status: ', 'mp_stacks_mailchimp' ) . $response_1['status'];
+	}
+
+	// The user's status is 404 so we know they are not on this list
+	$json = json_encode( array(
+		'email_address' => $data['email'],
+		'status'        => 'pending', // "subscribed","unsubscribed","cleaned","pending"
+	) );
+
+	$ch = curl_init($url);
+
+	curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['content-type: application/json']);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+
+	$result = curl_exec($ch);
+	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+
+	$result = json_decode( $result, true );
+
+	if ( $result['status'] == 'pending' ){
+		return __( 'Check your email to confirm!', 'mp_stacks_mailchimp' );
+	}
+
+	if ( $response_1['status'] == 'subscribed' ){
+		return array(
+			'error' => __( 'You are already subscribed to this list', 'mp_stacks_mailchimp' )
+		);
+	}
+
+	if ( $response_1['status'] == 'unsubscribed' ){
+		return array(
+			'error' => __( 'You unsubscribed from this list. Unfortunately, you can not re-join it.', 'mp_stacks_mailchimp' )
+		);
+	}
+
+	return __( 'Something went wrong. Please try again.', 'mp_stacks_mailchimp' );
+
+ }
